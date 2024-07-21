cc88d9f4f1622e52af19514940de1fae9421af46
Joel Ethan Batac <bjoelethan@gmail.com>
2024-07-21 18:39:20 +0800

    Implement Dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,10 @@ function App() {
   const [input, setInput] = useState("")
 
   return (
-    <>
+    <body className="dark:bg-black dark:text-white  h-screen w-screen">
       <Navbar/>
-      <input type="text" value={input} onChange={(e) => setInput(e.target.value)} className="border-blue-50 border-8"/> 
-    </>
+      <input type="text" value={input} onChange={(e) => setInput(e.target.value)} className="border-blue-50 border-8 text-black"/> 
+    </body>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,12 @@
+import ToggleDark from "./ToggleDark"
+
 function Navbar(){
     return (
-        <header className="flex h-16 w-full items-center justify-between bg-background px-4 md:px-6 shadow-lg shadow-sm mb-5">
+        <header className="flex h-16 w-full items-center justify-between bg-background px-4 md:px-6 shadow-lg shadow-sm shadow-slate-500 mb-5 dark:shadow-slate-200 ">
           
             
-            <span className="text-lg font-semibold flex flex-row gap-3"><MountainIcon className="h-6 w-6" /> UTF-fy</span>
+            <span className="text-lg font-semibold flex flex-row gap-3 dark:text-white text-black"><MountainIcon className="h-6 w-6 text-black dark:text-white" /> CSARCH2 Simulation Project - Unicode Converter </span>
+            <ToggleDark className="dark:text-white"/>    
 
 
         </header>
