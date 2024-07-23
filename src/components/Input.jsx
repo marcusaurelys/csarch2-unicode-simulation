45c7aaf11fbb645a4645f3399945d0a0a69fdd89
Joel Ethan Batac <bjoelethan@gmail.com>
2024-07-23 14:18:55 +0800

    Change input to textarea to make character input more accurate

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,16 +1,17 @@
-function Input({label, type="text", placeholder="enter text", value, onChange, disabled}){
+function Input({label, type="text", placeholder="enter text", value, onChange, disabled, decoration}){
     
     
     return (
             <div className="mb-4">
             {label && <label className="block text-black text-sm font-bold dark:text-white">{label}</label>}
-              <input
+              <textarea
                 type={type}
                 placeholder={placeholder}
                 value={value}
                 onChange={onChange}
-                className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-black dark:focus:ring-white dark:focus:border-white focus:border-black dark:text-white dark:bg-black dark:border-white"
+                className={"w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-black dark:focus:ring-white dark:focus:border-white focus:border-black dark:text-white dark:bg-black dark:border-white h-10 overflow-hidden resize-none"}
                 disabled={disabled}
+                
               />
             </div>
     );
